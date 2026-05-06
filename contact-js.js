75fae2c2c72cce16c77a3f91fec74355c01d37d8
mohamed tarek <20241701629@cis.asu.edu.eg>
2026-05-06 18:06:29 +0300

    Update contact-js.js

--- contact-js.js
+++ contact-js.js
@@ -4,18 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (menuToggle && navLinks) {
         menuToggle.addEventListener('click', () => {
-            // Toggle the menu visibility[cite: 21, 22]
             navLinks.classList.toggle('active');
-        });
-    }
-
-    // Form Handling[cite: 21]
-    const contactForm = document.querySelector('.contact-form');
-    if (contactForm) {
-        contactForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            alert("Message Sent!");
-            contactForm.reset();
         });
     }
 });
